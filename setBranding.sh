4caf5815deedc947cfbf7688bb3cba39d35548c6
Saurin Dashadia <saurindashadia@gmail.com>
2024-06-11 17:37:21 +0530

    Updated script with backup option.

--- setBranding.sh
+++ setBranding.sh
@@ -5,5 +5,12 @@ if [ `id -u` -ne 0 ]
   exit
 fi
 
+#copy old daufault page for backup
+echo "Backing up your default page for new site...\n"
+cp /usr/local/CyberCP/index.html ./old-branding/NewSite.html
+echo "Backup done.\n\n"
+
 #copy default html file for new site
+echo "Setting up branded default page for new site...\n"
 cp ./branding/NewSite.html /usr/local/CyberCP/index.html
+echo "New page applied.\n\n"
